95d06733b9ca087677b60e5a1a07f49cd318f52c
Banu <sazminthasneema@gmail.com>
2022-06-13 06:58:27 +0530

    git comminting with intelj with bootstrap and logs and javadocs

--- src/main/java/lk/abcd/demoae2project/service/ContactService.java
+++ src/main/java/lk/abcd/demoae2project/service/ContactService.java
@@ -3,41 +3,86 @@ package lk.abcd.demoae2project.service;
 
 import lk.abcd.demoae2project.model.Contact;
 import lk.abcd.demoae2project.repository.ContactRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+/**This class contains methods for contact **/
 @Service
 public class ContactService {
     @Autowired
     private ContactRepository contactrepositoryobject;
-
+    /**
+     * Creating LOG
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(ContactService.class);
+    /**
+     * <Creating java Docs>
+     * This method returns all the contacts
+     * @return List<Contact>
+     */
     public  List<Contact> findAll(){
-        return contactrepositoryobject.findAll();
+        List<Contact> contacts = contactrepositoryobject.findAll();
+        LOG.info("Successfully retrieved all the contact details");
+        return contacts;
     }
 
+    /**
+     * This method returns contact for a given id
+     * @param id
+     * @return Optional<Contact>
+     */
     public Optional<Contact> findById(int id){
-        return contactrepositoryobject.findById(id);
+        Optional<Contact> contact = contactrepositoryobject.findById(id);
+        LOG.info("Successfully retrieved contact details for the given id");
+        return contact;
     }
 
+    /**
+     * LOG.info is added for the return functions not for void functions
+     *
+     * this method saves a contact and returns it
+     * @param contactobject
+     * @return Contact
+     */
     public Contact saveContact(Contact contactobject){
-        return contactrepositoryobject.save(contactobject);
+        Contact savedContact = contactrepositoryobject.save(contactobject);
+        LOG.info("successfully saved contact for the given details.");
+        return savedContact;
     }
+
+    /**
+     * This method updates a contact of given id
+     * @param id
+     * @param contact
+     * @return
+     */
     public Contact updatecontact(int id, Contact contact) {
+        LOG.info("updating contact for a given id.");
         Contact updatecontact = contactrepositoryobject.findById(id).orElse(null);
         if (updatecontact != null){
             updatecontact.setName(contact.getName());
             updatecontact.setEmail(contact.getEmail());
             updatecontact.setCountry(contact.getCountry());
             contactrepositoryobject.save(updatecontact);
+        }else{
+            LOG.warn("No contact details for the given id");
         }
         return updatecontact;
 
     }
+
+    /**
+     * This method deletes a contact for given id
+     * @param id
+     */
     public void deleteById(int id){
         contactrepositoryobject.deleteById(id);
+        LOG.info("Successfully deleted the details of the given id.");
     }
 
 }
--- src/main/java/lk/abcd/demoae2project/service/HotelService.java
+++ src/main/java/lk/abcd/demoae2project/service/HotelService.java
@@ -1,30 +1,69 @@
 package lk.abcd.demoae2project.service;
 
+import lk.abcd.demoae2project.model.Feedback;
 import lk.abcd.demoae2project.model.Hotel;
 import lk.abcd.demoae2project.repository.HotelRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+/**
+ *This class contains methods for Hotel
+ */
 @Service
 public class HotelService {
     @Autowired
     private HotelRepository hotelRepositoryobject;
-
+    /**
+     * Creating LOG
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(HotelService.class);
+    /**
+     *
+     *This method returns all the Hotel
+     * @return List<Hotel>
+     */
     public List<Hotel> findAll(){
-        return hotelRepositoryobject.findAll();
+        List<Hotel> hotels = hotelRepositoryobject.findAll();
+        LOG.info("Successfully retrieved all the hotel details");
+        return hotels;
     }
 
+    /**
+     *This method returns hotel for a given id
+     * @param id
+     * @return Optional<Hotel>
+     */
     public Optional<Hotel> findById(int id){
-        return hotelRepositoryobject.findById(id);
+        Optional<Hotel> hotel = hotelRepositoryobject.findById(id);
+        LOG.info("Successfully retrieved hotel details for the given id");
+        return hotel;
     }
 
+    /**
+     * this method saves a hotel and returns it
+     * @param hotelobject
+     * @return
+     */
     public Hotel saveHotel(Hotel hotelobject){
-        return hotelRepositoryobject.save(hotelobject);
+        Hotel savedHotel = hotelRepositoryobject.save(hotelobject);
+        LOG.info("successfully saved hotel for the given details.");
+        return savedHotel;
     }
+
+    /**
+     *
+     * This method updates a hotel of given id
+     * @param id
+     * @param hotel
+     * @return
+     */
     public Hotel updatehotel(int id, Hotel hotel) {
+        LOG.info("updating hotel for a given id.");
         Hotel updatehotel = hotelRepositoryobject.findById(id).orElse(null);
         if (updatehotel != null){
             updatehotel.setName(hotel.getName());
@@ -33,12 +72,18 @@ public class HotelService {
             updatehotel.setCountry(hotel.getCountry());
             updatehotel.setNumberofemployees(hotel.getNumberofemployees());
             hotelRepositoryobject.save(updatehotel);
+        }else{
+            LOG.warn("No hotel details for the given id");
         }
         return updatehotel;
-
     }
+    /**
+     *This method deletes a hotel for given id
+     * @param id
+     */
     public void deleteById(int id){
         hotelRepositoryobject.deleteById(id);
+        LOG.info("Successfully deleted the details of the given id.");
     }
 
 }
