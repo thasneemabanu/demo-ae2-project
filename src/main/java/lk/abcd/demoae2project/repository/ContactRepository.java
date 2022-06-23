34ed116968b5fc22b08d55de47acabeef85810e7
Umar Qyyam <mohamedumarferoze07700@gmail.com>
2022-06-23 10:42:09 +0530

    Edits

--- src/main/java/lk/abcd/demoae2project/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package lk.abcd.demoae2project.repository;
-
-import lk.abcd.demoae2project.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository <Contact, Integer> {
-}
